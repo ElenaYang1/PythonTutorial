e30f820a84abf4b4b656a97cbbc1195e8b903f82
Elena Yang <elenayyy0906@yahoo.com>
2021-07-15 21:21:32 -0400

    wrote block letters project

---
+++ projects/basicPython/blockLetters.py
@@ -0,0 +1,41 @@
+myName = """
+EEEEE
+E
+E
+EEE
+E
+EEEEE
+
+L
+L
+L
+L
+L
+L
+LLLLL
+
+EEEEE
+E
+E
+EEE
+E
+EEEEE
+
+N          N
+NN        N
+N N      N
+N  N    N
+N   N  N
+N    N N
+N     N
+
+  A
+ A  A
+A    A
+AAAAAA
+A     A
+A     A
+A     A
+
+"""
+print(myName)
